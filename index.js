15dcb1769f025d8c063ef171bace65b7623e7c96
sbelisconi <42112228+sbelisconi@users.noreply.github.com>
2022-10-12 00:55:20 +0100

    Added a reset button to reset the scores to 0

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.getElementById("homescore").textContent = score1
 document.getElementById("awayscore").textContent = score2
 let homescores = document.getElementById("homescore")
 let awayscores = document.getElementById("awayscore")
+let setStart = document.getElementById("btn-reset")
 
 function homePlusOne() {
     score1 += 1
@@ -38,5 +39,12 @@ function awayPlusTwo() {
 function awayPlusThree() {
     score2 += 3
     console.log(score1)
+    awayscores.textContent = score2
+}
+
+function reset() {
+    score1 = 0
+    score2 = 0
+    homescores.textContent = score1
     awayscores.textContent = score2
 }
